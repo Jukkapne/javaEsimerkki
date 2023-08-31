016f2685a335e441f88a731ac001f8c89250325a
Jukkapne <jukka.nevalainen@oamk.fi>
2023-08-31 20:08:20 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // Create a new instance of the Person class
+        Person person1 = new Person("John", "Doe", 25);
+        // Call the method from the Person class
+        person1.greet();
+        
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    // Define the properties of the Person class
+    String firstName;
+    String lastName;
+    int age;
+
+    // Define the constructor of the Person class
+    public Person(String firstName, String lastName, int age) {
+        // Set the properties of the Person class
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // Define the greet method of the Person class
+    public void greet() {
+        System.out.println("Hello, my name is " + this.firstName + " " + this.lastName + " and I am " + this.age + " years old.");
+    }
+}
