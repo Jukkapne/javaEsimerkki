fe4abd626f112448d84cec17789b92a2dde108d3
Jukkapne <jukka.nevalainen@oamk.fi>
2023-09-06 10:01:08 +0300

    uutta

--- src/Person.java
+++ src/Person.java
@@ -1,19 +1,18 @@
 public class Person {
     // Define the properties of the Person class
     String firstName;
-    String lastName;
     int age;
 
     // Define the constructor of the Person class
-    public Person(String firstName, String lastName, int age) {
+    public Person(String firstName, int age) {
         // Set the properties of the Person class
         this.firstName = firstName;
-        this.lastName = lastName;
+       
         this.age = age;
     }
 
     // Define the greet method of the Person class
     public void greet() {
-        System.out.println("Moi, my name is " + this.firstName + " " + this.lastName + " and I am " + this.age + " years old.");
+        System.out.println("Moi, my name is " + this.firstName + " " + " and I am " + this.age + " years old.");
     }
 }
